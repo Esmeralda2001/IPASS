7809b1aa01ef843b76fb0ba2b97dd0c3c5ddc7d1
Esmeralda <esmeralda.dewolf@student.hu.nl>
2019-06-26 15:47:12 +0200

    ClassCities: added docstrings

--- ClassCities.py
+++ ClassCities.py
@@ -1,24 +1,32 @@
 import math
 
 class City:
-    #constructor function of class 'City'
-    #city has the following properties: name, x, y
-    #name is a string
-    #x and y are the coordinates of the city
+
     def __init__(self, name, posx, posy):
+        '''
+        :param name: string, name of the city
+        :param posx: integer, x-coordinate of the city
+        :param posy: integer, y-coordinate of the city
+        '''
         self.name = name
         self.x = posx
         self.y = posy
         self.neighbors = []
 
-    #function distance calculates the distance between
-    #a city and another city
     def distance(self, city):
+        '''
+        :param city: city is a part of the City Class
+        :return: the distance between 'self' and another instance of the City Class
+        '''
         xAxis = (self.x-city.x)**2
         yAxis = (self.y-city.y)**2
         dist = math.sqrt(xAxis+yAxis)
         return dist
 
     def addNeighbor(self, city):
+        '''
+        :param city: city is part of the City Class
+        :return: returns void, function simply adds an instance of the City Class to self.neighbors
+        '''
         self.neighbors.append(city)
 
